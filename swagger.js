fcdd5c845eec90306b71ac55537cf59f3eadafd8
Cleverson Martins <clev.mts@gmail.com>
2022-02-10 14:32:22 -0300

    Projeto Comercial

---
+++ swagger.js
@@ -0,0 +1,42 @@
+// importação do módulo do swagger-autogen para gerar o conteúdo da
+//documentação dentro do arquivo swagger_output.json e gerar a página da doc
+const swaggerAutogen = require("swagger-autogen")();
+
+const output = "./swagger_output.json";
+const endpoints = ["./endpoints_usuarios.js"];
+
+// informações complementares da documentação
+const doc = {
+        info:{
+        version:"1.0.0",
+        title:"Projeto Comercial",
+        description:"Projeto desenvolvido para estudo de FullStack",
+        contact:{
+            
+            name:"Cleverson Martins",
+            url:"https://github.com/clkever",
+            email:"clev.mts@gmail.com"
+        }
+
+    },
+
+    host:"localhost:5000/usuarios",
+    basePath:"/",
+    schemes:["http","https"],
+    consumes:["application/json"],
+
+    tags:[
+
+        {
+            name:"Usuários",
+            description:"Você pode testar as rotas dos usuário abaixo"
+
+        },
+
+    ],
+
+};
+
+swaggerAutogen(output,endpoints.doc).then(()=>{
+    require("./src/main/index.js");
+});
